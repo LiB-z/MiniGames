3813986bc33cbf3c69d003ffb33cd8bb772d147a
lbereza <lbereza@innoseti.ru>
2026-09-20 23:39:01 +0200

    chore: configure Prettier

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 Web application with 2 pages and 2 dialog windows based on the [design mockup](https://www.figma.com/design/4MnLizE59gZI2DDxaSgZqi/MiniGames?node-id=0-1&m=dev&t=fsxihMHW5MYSxEkW-1):
-* Home
-* Library
-* Auth modal (sign in / sign up)
-* Game Details modal (game details)  
+
+- Home
+- Library
+- Auth modal (sign in / sign up)
+- Game Details modal (game details)
 
 The project build a full UI using only TS + HTML + SCSS, without frameworks or ready-made UI libraries.
--- eslint.config.ts
+++ eslint.config.ts
@@ -1,11 +1,18 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser }, linterOptions: { noInlineConfig: true }, rules: {
-      "@typescript-eslint/no-explicit-any": "error",
-    }},
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser },
+    linterOptions: { noInlineConfig: true },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+    },
+  },
   tseslint.configs.recommended,
 ]);
